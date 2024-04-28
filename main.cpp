1d0a3df20a163ac76632322fe165cff70cf5a98c
happysmileyface <95998572+happysmileyface@users.noreply.github.com>
2024-04-27 22:16:57 -0700

    Add files via upload
    now calculates addresses of other values from offsets of the score

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
+/*
+TODO:
+  put the code that i copy-pasted into either a header or DLL
+  make win32 window for interface instead of console
+  get around ALSR for getting scoreAddress without manual searching
+  figure out why the threads have a weird sleep time sometimes
+*/
+#define puyoYCoordOffset -2672
 #include <iostream>
 #include <conio.h>
 #include <thread>
 #include <chrono>
 #include <windows.h>
+#include <math.h>
 using namespace std;
 bool continue_counting = true;
 bool continue_running = true;
 short timecount = 0;
 char input;
+unsigned long long scoreAddress; // use the address of the score for offsets
 
 HANDLE hProc = NULL;
 HWND Window = NULL;
@@ -15,12 +25,24 @@ HHOOK hHook = NULL;
 HANDLE hThread = NULL;
 DWORD procID;
 
+inline void hexStringToValue(string s){
+  short current;
+  for (short i = 1; i < 8; i++){
+    current = (short)s[i]-48;
+    if (current > 9) current -= 7;
+    scoreAddress += current * pow(16,(8-i-1));
+    std::cout << "current letter: " << s[i] << " and its val: " << current;
+    std::cout << "scoreAddress: " << scoreAddress << std::endl;
+  }
+  std::cout << " | converted to int -> " << scoreAddress << std::endl;
+}
+
 void Tcounting(){
   unsigned long Value = 0;
   while (continue_running){ 
     if (continue_counting){
       Value++;
-      WriteProcessMemory(hProc,(BYTE*)0x0703A070,&Value,sizeof(Value),NULL);
+      WriteProcessMemory(hProc,(BYTE*)scoreAddress,&Value,sizeof(Value),NULL);
       std::this_thread::sleep_for(std::chrono::seconds(1));
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -40,13 +62,13 @@ void Twaitforstop(){
 
 
 void hack(){ 
-  //const BYTE Y = 0x0A;
-  const BYTE timer = 0x00;
+  const BYTE Y = 0x0A;
+  //const BYTE timer = 0x00; un-comment when not using the height reset
     while (continue_running)
     {
       if (continue_counting){
-        //WriteProcessMemory(hProc,(BYTE*)0x07039600,&Y,sizeof(Y),NULL);
-        WriteProcessMemory(hProc,(BYTE*)0x07039663,&timer,sizeof(timer),NULL);
+        WriteProcessMemory(hProc,(BYTE*)(scoreAddress+puyoYCoordOffset),&Y,sizeof(Y),NULL);
+        //WriteProcessMemory(hProc,(BYTE*)0x07039663,&timer,sizeof(timer),NULL);
         std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
       }
       else std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -56,30 +78,31 @@ void hack(){
 int main(){
   std::cout << "working\n"; 
 
+ //start of obviously copy-pasted code from nullbyte 
+  Window = FindWindow(NULL,"PuyoVS");
  
-    Window = FindWindow(NULL,"PuyoVS");
- 
-    if(!Window){
-        cout << "Window not found" << endl;
-        return 1;
+  if(!Window){
+    std::cout << "Window not found" << std::endl;
+    return 1;
+  }
+  else {
+    if (!GetWindowThreadProcessId(Window,&procID)){
+      std::cerr << "Get window process ID error: " << GetLastError() << std::endl;
+      return 1;
     }
-    else {
-        if (!GetWindowThreadProcessId(Window,&procID)){
-            cerr << "Get window process ID error: " << GetLastError() << endl;
-            return 1;
-        }
-    }
- 
-    if(procID != 0) hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
- 
-    if(!hProc){
-        cerr << "Can not open process: " << GetLastError() << endl;
-        return 2;
-    }
-    else{
-        cout << "Process Found Ready to write\n" << endl;
-    } 
+  }
+  if(procID != 0) hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
+  if(!hProc){
+    std::cerr << "Can not open process: " << GetLastError() << std::endl;
+    return 2;
+  }
+  else std::cout << "Process Found Ready to write\n" << std::endl;
+  //end of copied code
   
+  std::cout << "address of score: ";
+  string hexString = "";
+  std::cin >> hexString;
+  hexStringToValue(hexString);
   std::thread CounterThread (Tcounting);
   std::thread StopperThread (Twaitforstop);
   std::thread hackThread (hack);
